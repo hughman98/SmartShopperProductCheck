b39c40216282bd352cb10e1a1ad0684d152b041c
hughman98 <30803969+hughman98@users.noreply.github.com>
2021-10-16 21:29:11 -0400

    Updated URLs
    Changed it so that it's not necessary to go to the "product_check" page to search.

--- ProductCheck/ProductCheck/urls.py
+++ ProductCheck/ProductCheck/urls.py
@@ -19,5 +19,6 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('product_check/', include('product_check.urls'))
+    path('product_check/', include('product_check.urls')),
+    path('', include('product_check.urls'))
 ]
